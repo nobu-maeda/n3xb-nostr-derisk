845f36f9908fd5f49f4f99632eda81fe46fc562f
Nobu Maeda <nobu.maeda@proton.me>
2023-07-19 23:20:12 -0400

    Mutex lock between user input and Nostr responses

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,45 @@
 use futures::executor::block_on;
+use nostr_sdk::prelude::*;
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
-use std::string::{String,ToString};
-use std::sync::{Arc,Mutex};
+use std::string::{String, ToString};
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 use strum_macros::Display;
-use nostr_sdk::prelude::*;
 
 const APP_SPEC_KIND_SUFFIX: u16 = 30078;
 const APP_SPEC_D_TAG: &str = "n3x";
 
 type ArcClient = Arc<Mutex<Client>>;
+type ArcMutex = Arc<Mutex<i32>>;
 
 #[tokio::main]
 async fn main() {
     let my_keys: Keys = Keys::generate();
-    let listen_client = init_nostr_client(&my_keys).await;
+    let mutex = Arc::new(Mutex::new(0));
     let client = init_nostr_client(&my_keys).await;
+
+    let listen_client = init_nostr_client(&my_keys).await;
     start_notification_listening(&listen_client).await;
 
-    let thread_client = listen_client.clone();
-    let handle = thread::spawn(move || {
-        block_on(perform_notification_handling(&thread_client));
+    let thread_mutex = mutex.clone();
+    let thread_listen_client = listen_client.clone();
+    let thread_post_client: Arc<Mutex<Client>> = client.clone();
+
+    _ = thread::spawn(move || {
+        block_on(perform_notification_handling(
+            &thread_mutex,
+            &thread_listen_client,
+            &thread_post_client,
+        ));
     });
 
     println!("n3x Nostr Derisk CLI");
 
     // listen and process subscriptions
 
-     loop {
+    loop {
         println!("=> Options");
         println!("  1. Create New Offer");
         println!("  2. Show Outstanding Offers");
@@ -38,22 +48,27 @@ async fn main() {
 
         let user_input = get_user_input();
 
-        match user_input.as_str() {
-            "1" => create_offer(&client).await,
-            "2" => show_offers(&client).await,
-            "3" => take_offer(&client).await,
-            _ => println!("Invalid input. Please input a number."),
+        {
+            let mut lock = mutex.lock().unwrap();
+            *lock = 0;
+            match user_input.as_str() {
+                "1" => create_offer(&client).await,
+                "2" => show_offers(&client).await,
+                "3" => take_offer(&client).await,
+                _ => println!("Invalid input. Please input a number."),
+            }
         }
-
         println!("");
     }
-    handle.join().unwrap();
 }
 
 // Common Util
 
 async fn init_nostr_client(keys: &Keys) -> ArcClient {
-    let opts = Options::new().wait_for_connection(true).wait_for_send(true).difficulty(8);
+    let opts = Options::new()
+        .wait_for_connection(true)
+        .wait_for_send(true)
+        .difficulty(8);
     let client = Client::new_with_opts(&keys, opts);
 
     client.add_relay("ws://localhost:8008", None).await.unwrap();
@@ -75,7 +90,10 @@ fn get_user_input() -> String {
 async fn start_notification_listening(client: &ArcClient) {
     // Offer subscription
     let mut custom_tag_filter = Map::new();
-    custom_tag_filter.insert("#d".to_string(), Value::Array(vec![Value::String(APP_SPEC_D_TAG.to_string())]));
+    custom_tag_filter.insert(
+        "#d".to_string(),
+        Value::Array(vec![Value::String(APP_SPEC_D_TAG.to_string())]),
+    );
 
     let offer_subscription = Filter::new()
         .since(Timestamp::now())
@@ -84,8 +102,8 @@ async fn start_notification_listening(client: &ArcClient) {
 
     // Order subscription
     let order_subscription = Filter::new()
-    .since(Timestamp::now())
-    .pubkey(client.lock().unwrap().keys().public_key());
+        .since(Timestamp::now())
+        .pubkey(client.lock().unwrap().keys().public_key());
 
     // Confirmation subscription
 
@@ -93,29 +111,82 @@ async fn start_notification_listening(client: &ArcClient) {
     client.lock().unwrap().subscribe(filters).await;
 }
 
+async fn perform_notification_handling(
+    mutex: &ArcMutex,
+    listen_client: &ArcClient,
+    _post_client: &ArcClient,
+) {
+    let unlocked_client = listen_client.lock().unwrap();
+    unlocked_client
+        .handle_notifications(|notification| async {
+            let mut lock = mutex.lock().unwrap();
 
-async fn perform_notification_handling(client: &ArcClient) {
-    client.lock().unwrap().handle_notifications(|notification| {
-        match notification {
-            RelayPoolNotification::Event(url, event) => {
-                println!("Got Event Notification");
-                println!("URL: {}", url.as_str());
-                println!("Event: {}", event.as_json());
-                println!();
-            },
-            RelayPoolNotification::Message(_, _) => {
-                // println!("Got Message Notification");
-                // println!("URL: {}", url.as_str());
-                // println!("Message: {}", message.as_json());
-                // println!();
-            },
-            RelayPoolNotification::Shutdown => {
-                println!("Got Shutdown Notification");
-                println!();
-            },
-        };
-        Ok(())
-    }).await.unwrap();
+            match notification {
+                RelayPoolNotification::Event(url, event) => {
+                    println!();
+                    println!("Got Event Notification");
+                    println!("URL: {}", url.as_str());
+                    println!("Event: {}", event.as_json());
+                    println!();
+
+                    process_event_message(&event, &unlocked_client).await;
+                }
+                RelayPoolNotification::Message(_, _) => {
+                    // println!();
+                    // println!("Got Message Notification");
+                    // println!("URL: {}", url.as_str());
+                    // println!("Message: {}", message.as_json());
+                    // println!();
+                }
+                RelayPoolNotification::Shutdown => {
+                    println!();
+                    println!("Got Shutdown Notification");
+                    println!();
+                }
+            };
+            *lock = 1;
+            Ok(())
+        })
+        .await
+        .unwrap();
+}
+
+async fn process_event_message(event: &Event, client: &Client) {
+    if let Kind::EncryptedDirectMessage = event.kind {
+        process_direct_message(event, &client).await;
+    }
+}
+
+async fn process_direct_message(event: &Event, client: &Client) {
+    if let Ok(msg) = decrypt(
+        &client.keys().secret_key().unwrap(),
+        &event.pubkey,
+        &event.content,
+    ) {
+        println!("New DM: {}", msg);
+        let header = &msg[0..10];
+        if header == "n3x_header" {
+            process_n3x_direct_message(&msg[10..], &client).await;
+        }
+    } else {
+        println!("Failed to decrypt direct message");
+    }
+}
+
+async fn process_n3x_direct_message(content: &str, _client: &Client) {
+    let n3x_message: N3xMessage = serde_json::from_str(content).unwrap();
+    match n3x_message {
+        N3xMessage::Order(order_content) => process_n3x_order(&order_content).await,
+        N3xMessage::Confirm(confirm_content) => process_n3x_confirmation(&confirm_content).await,
+    }
+}
+
+async fn process_n3x_order(content: &OrderContent) {
+    println!("{:?}", content);
+}
+
+async fn process_n3x_confirmation(content: &ConfirmContent) {
+    println!("{:?}", content);
 }
 
 // Create Offer
@@ -129,7 +200,7 @@ enum OfferDirection {
 #[derive(Debug, Deserialize, Serialize)]
 struct OfferContent {
     quantity: u64, // in sats
-    price: i64, // in sats / dollar
+    price: i64,    // in sats / dollar
 }
 
 fn buy_sell_string_to_enum(user_input: String) -> Option<OfferDirection> {
@@ -137,7 +208,7 @@ fn buy_sell_string_to_enum(user_input: String) -> Option<OfferDirection> {
     match user_input_lowercase.as_str() {
         "buy" | "buying" => Some(OfferDirection::Buy),
         "sell" | "selling" => Some(OfferDirection::Sell),
-        _ => None
+        _ => None,
     }
 }
 
@@ -145,9 +216,11 @@ fn get_offer_dir() -> OfferDirection {
     loop {
         let user_input = get_user_input();
         if let Some(offer_dir) = buy_sell_string_to_enum(user_input) {
-            return offer_dir
+            return offer_dir;
         } else {
-            println!("Unrecognized input. Please specify either 'Buy', 'Buying', 'Sell', or 'Selling'");
+            println!(
+                "Unrecognized input. Please specify either 'Buy', 'Buying', 'Sell', or 'Selling'"
+            );
         }
     }
 }
@@ -156,7 +229,7 @@ fn get_quantity() -> u64 {
     loop {
         let user_input = get_user_input();
         if let Ok(quantity) = user_input.parse() {
-            return quantity
+            return quantity;
         } else {
             println!("Unrecognized input. Please input a positive integer");
         }
@@ -167,7 +240,7 @@ fn get_price() -> i64 {
     loop {
         let user_input = get_user_input();
         if let Ok(price) = user_input.parse() {
-            return price
+            return price;
         } else {
             println!("Unrecognized input. Please input an integer");
         }
@@ -175,16 +248,31 @@ fn get_price() -> i64 {
 }
 
 async fn send_offer(offer_dir: OfferDirection, quantity: u64, price: i64, client: &ArcClient) {
-    let app_tag: Tag = Tag::Generic(TagKind::Custom("d".to_string()), vec![APP_SPEC_D_TAG.to_string()]);
-    let dir_tag: Tag = Tag::Generic(TagKind::Custom("x".to_string()), vec![offer_dir.to_string()]);
+    let app_tag: Tag = Tag::Generic(
+        TagKind::Custom("d".to_string()),
+        vec![APP_SPEC_D_TAG.to_string()],
+    );
+    let dir_tag: Tag = Tag::Generic(
+        TagKind::Custom("x".to_string()),
+        vec![offer_dir.to_string()],
+    );
     let event_tags = [app_tag, dir_tag];
 
     let offer_content = OfferContent { quantity, price };
     let offer_content_string = serde_json::to_string(&offer_content).unwrap();
 
     let my_keys = client.lock().unwrap().keys();
-    let builder = EventBuilder::new(Kind::ParameterizedReplaceable(APP_SPEC_KIND_SUFFIX), offer_content_string, &event_tags);
-    client.lock().unwrap().send_event(builder.to_event(&my_keys).unwrap()).await.unwrap();
+    let builder = EventBuilder::new(
+        Kind::ParameterizedReplaceable(APP_SPEC_KIND_SUFFIX),
+        offer_content_string,
+        &event_tags,
+    );
+    client
+        .lock()
+        .unwrap()
+        .send_event(builder.to_event(&my_keys).unwrap())
+        .await
+        .unwrap();
 }
 
 async fn create_offer(client: &ArcClient) {
@@ -205,19 +293,32 @@ async fn show_offers(client: &ArcClient) {
 
     // Kind, d = n3xB, offerDirection
     let mut custom_tag_filter = Map::new();
-    custom_tag_filter.insert("#d".to_string(), Value::Array(vec![Value::String(APP_SPEC_D_TAG.to_string())]));
-    custom_tag_filter.insert("#x".to_string(), Value::Array(vec![Value::String(offer_dir.to_string())]));
+    custom_tag_filter.insert(
+        "#d".to_string(),
+        Value::Array(vec![Value::String(APP_SPEC_D_TAG.to_string())]),
+    );
+    custom_tag_filter.insert(
+        "#x".to_string(),
+        Value::Array(vec![Value::String(offer_dir.to_string())]),
+    );
 
     let subscription = Filter::new()
-         .kind(Kind::ParameterizedReplaceable(APP_SPEC_KIND_SUFFIX))
-         .custom(custom_tag_filter);
-    
-     let timeout = Duration::from_secs(1);
-     let events = client.lock().unwrap()
-         .get_events_of(vec![subscription], Some(timeout))
-         .await.unwrap();
+        .kind(Kind::ParameterizedReplaceable(APP_SPEC_KIND_SUFFIX))
+        .custom(custom_tag_filter);
 
-    println!("{} n3xB {} offers found", events.len(), offer_dir.to_string());
+    let timeout = Duration::from_secs(1);
+    let events = client
+        .lock()
+        .unwrap()
+        .get_events_of(vec![subscription], Some(timeout))
+        .await
+        .unwrap();
+
+    println!(
+        "{} n3xB {} offers found",
+        events.len(),
+        offer_dir.to_string()
+    );
     for event in events {
         println!("{:?}", event.as_json());
     }
@@ -226,19 +327,47 @@ async fn show_offers(client: &ArcClient) {
 // Take Offer (Send Order)
 
 #[derive(Debug, Deserialize, Serialize)]
+enum N3xMessage {
+    Order(OrderContent),
+    Confirm(ConfirmContent),
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 struct OrderContent {
     offer_id: String,
     quantity: u64, // in sats
-    price: i64, // in sats / dollar
+    price: i64,    // in sats / dollar
 }
 
-async fn send_order(pubkey_string: String, offer_id: String, quantity: u64, price: i64, client: &ArcClient) {
+#[derive(Debug, Deserialize, Serialize)]
+struct ConfirmContent {
+    order_id: String,
+    status_code: String,
+}
+
+async fn send_order(
+    pubkey_string: String,
+    offer_id: String,
+    quantity: u64,
+    price: i64,
+    client: &ArcClient,
+) {
     let pubkey = XOnlyPublicKey::from_str(pubkey_string.as_str()).unwrap();
 
-    let order_content = OrderContent { offer_id, quantity, price };
-    let order_content_string = serde_json::to_string(&order_content).unwrap();
-
-    client.lock().unwrap().send_direct_msg(pubkey, order_content_string).await.unwrap();
+    let order_content = OrderContent {
+        offer_id,
+        quantity,
+        price,
+    };
+    let n3x_message = N3xMessage::Order(order_content);
+    let order_content_string = serde_json::to_string(&n3x_message).unwrap();
+    let json_string = "n3x_header".to_owned() + order_content_string.as_str();
+    client
+        .lock()
+        .unwrap()
+        .send_direct_msg(pubkey, json_string)
+        .await
+        .unwrap();
 }
 
 async fn take_offer(client: &ArcClient) {
@@ -256,8 +385,8 @@ async fn take_offer(client: &ArcClient) {
 // Respond to Order
 
 // async fn respond_to_order(client: &Client) {
-    // pubkey
-    // order-id
-    // Yes or No (with reason, basically if the Offer existed)
-    // send DM
+// pubkey
+// order-id
+// Yes or No (with reason, basically if the Offer existed)
+// send DM
 // }
